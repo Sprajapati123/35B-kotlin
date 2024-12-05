8f18e87e6b6e25fc0ce923074f952478b15d06e2
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-05 09:29:33 +0545

    date picker

--- app/src/main/java/com/example/a35b/FormActivity.kt
+++ app/src/main/java/com/example/a35b/FormActivity.kt
@@ -1,21 +1,25 @@
 package com.example.a35b
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class FormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var spinner: Spinner
     lateinit var autoCompleteTextView: AutoCompleteTextView
     lateinit var display: TextView
+    lateinit var datePicker: EditText
     val countries = arrayOf(
         "Nepal", "China",
         "India", "US", "Canada", "Australia"
@@ -33,6 +37,13 @@ class FormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         spinner = findViewById(R.id.spinner)
         display = findViewById(R.id.displaySpinner)
         autoCompleteTextView = findViewById(R.id.autoComplete)
+        datePicker = findViewById(R.id.editTextDate)
+
+        datePicker.isFocusable = false
+        datePicker.isClickable = true
+        datePicker.setOnClickListener {
+            loadCalendar()
+        }
 
         val autoCompleteAdapter = ArrayAdapter(
             this@FormActivity,
@@ -58,6 +69,23 @@ class FormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun loadCalendar() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dialog = DatePickerDialog(
+            this@FormActivity,
+            DatePickerDialog.OnDateSetListener
+            { d, year, month, day ->
+                         //2024   05    01
+                datePicker.setText("$day/${month+1}/$year")
+            },year,month,day
+        )
+        dialog.show()
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, view: View?, p2: Int, p3: Long) {
