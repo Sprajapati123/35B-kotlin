5b69d42b842c1607ba3f873cb02581268f8b51f5
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-09 10:23:03 +0545

    creating sample file for recycler view

---
+++ app/src/main/java/com/example/a35b/RecyclerActivity.kt
@@ -0,0 +1,26 @@
+package com.example.a35b
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recycler)
+        recyclerView = findViewById(R.id.recyclerView)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
