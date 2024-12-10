63b4b3bc68f91ca6701238fb5ca682d06d487efa
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-10 10:16:47 +0545

    recycler

--- app/src/main/java/com/example/a35b/RecyclerActivity.kt
+++ app/src/main/java/com/example/a35b/RecyclerActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a35b.adapter.FruitsAdapter
 
 class RecyclerActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     var imageList = ArrayList<Int>()
     var fruitsTitle = ArrayList<String>()
     var fruitsDesc = ArrayList<String>()
+
+    lateinit var adapter: FruitsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,6 +35,15 @@ class RecyclerActivity : AppCompatActivity() {
         fruitsDesc.add("this is mango")
         fruitsDesc.add("this is apple")
 
+
+
+        adapter = FruitsAdapter(
+            this@RecyclerActivity,
+            imageList, fruitsTitle,fruitsDesc
+        )
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@RecyclerActivity)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/example/a35b/adapter/FruitsAdapter.kt
+++ app/src/main/java/com/example/a35b/adapter/FruitsAdapter.kt
@@ -9,31 +9,31 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.a35b.R
 import de.hdodenhof.circleimageview.CircleImageView
 
+
 class FruitsAdapter(
     val context: Context,
-    val imageList: ArrayList<Int>,
-    val fruitsTitle: ArrayList<String>,
-    val fruitsDesc: ArrayList<String>
-) : RecyclerView.Adapter<FruitsAdapter.FruitsViewHolder>() {
-    class FruitsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var image: CircleImageView = itemView.findViewById(R.id.profile_image)
-        var title: TextView = itemView.findViewById(R.id.lblFruitName)
-        var desc: TextView = itemView.findViewById(R.id.lblDescription)
+    val imageList : ArrayList<Int>,
+    val fruitsTitle : ArrayList<String>,
+    val imageDesc : ArrayList<String>,
+) : RecyclerView.Adapter<FruitsAdapter.FruitsViewHolder>(){
+    class FruitsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView : CircleImageView = itemView.findViewById(R.id.profile_image)
+        val title : TextView = itemView.findViewById(R.id.lblFruitName)
+        val desc : TextView = itemView.findViewById(R.id.lblDescription)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitsViewHolder {
-        val itemView : View = LayoutInflater.from(context).
-        inflate(R.layout.single_product,parent,false)
+        val itemView : View = LayoutInflater.from(context).inflate(R.layout.single_product,parent,false)
         return FruitsViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        return 3
+        return imageDesc.size
     }
 
     override fun onBindViewHolder(holder: FruitsViewHolder, position: Int) {
+        holder.desc.text = imageDesc[position]
         holder.title.text = fruitsTitle[position]
-        holder.desc.text = fruitsDesc[position]
-        holder.image.setImageResource(imageList[position])
+        holder.imageView.setImageResource(imageList[position])
     }
 }
