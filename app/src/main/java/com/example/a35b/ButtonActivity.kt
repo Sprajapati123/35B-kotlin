b334fd3532e3b8a4fe3599fd140599ace9df274d
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-12 09:53:25 +0545

    intnet

---
+++ app/src/main/java/com/example/a35b/ButtonActivity.kt
@@ -0,0 +1,53 @@
+package com.example.a35b
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a35b.databinding.ActivityButtonBinding
+
+class ButtonActivity : AppCompatActivity() {
+    lateinit var binding : ActivityButtonBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityButtonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            if(binding.editUsername.text.isEmpty()){
+                binding.editUsername.error = "username can't be empty"
+            }else if(binding.editPasswords.text.isEmpty()){
+                binding.editPasswords.error = "password can't be empty"
+            }else{
+                val intent = Intent(this@ButtonActivity,
+                    FormActivity::class.java)
+
+                val username: String = binding.editUsername.text.toString()
+                val password: String = binding.editPasswords.text.toString()
+
+                //key      //value
+                intent.putExtra("username",username)
+                intent.putExtra("password",password)
+                startActivity(intent)
+            }
+
+        }
+
+
+//        binding.btnNavigate.setOnClickListener {
+//           val intent = Intent(this@ButtonActivity,
+//               RecyclerActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
