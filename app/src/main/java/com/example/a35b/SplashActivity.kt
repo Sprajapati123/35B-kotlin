56579842563ea2da6ada2ced270f898f46b9ebb3
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-23 09:49:29 +0545

    splash

--- app/src/main/java/com/example/a35b/SplashActivity.kt
+++ app/src/main/java/com/example/a35b/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a35b
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,16 +12,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
+        sharedPreferences =
+            getSharedPreferences("userData",
+                Context.MODE_PRIVATE)
+
+        val username : String = sharedPreferences
+            .getString("username","").toString()
+
+        val password : String = sharedPreferences
+            .getString("password","").toString()
+
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@SplashActivity,
-                ButtonActivity::class.java)
-            startActivity(intent)
+            if(username.isEmpty()){
+                val intent = Intent(this@SplashActivity,
+                    ButtonActivity::class.java)
+                startActivity(intent)
+            }else{
+                val intent = Intent(this@SplashActivity,
+                    DestinationActivity::class.java)
+                startActivity(intent)
+            }
+
         },2000)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
