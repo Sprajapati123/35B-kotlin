7a6d37b41c05648fd882818ba5488301658e1039
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-24 09:49:12 +0545

    fragment

--- app/src/main/java/com/example/a35b/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/a35b/ui/activity/HomeActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.a35b.R
 import com.example.a35b.databinding.ActivityHomeBinding
+import com.example.a35b.ui.fragment.FirstFragment
+import com.example.a35b.ui.fragment.SecondFragment
 
 class HomeActivity : AppCompatActivity() {
     lateinit var binding : ActivityHomeBinding
@@ -16,9 +20,29 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnFirst.setOnClickListener {  }
+        binding.btnFirst.setOnClickListener {
+            val fragmentManager : FragmentManager =
+                supportFragmentManager
 
-        binding.btnSecond.setOnClickListener {  }
+            val fragmentTransaction : FragmentTransaction =
+                fragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.frame,FirstFragment())
+
+            fragmentTransaction.commit()
+
+        }
+
+        binding.btnSecond.setOnClickListener {
+            val fragmentManager : FragmentManager =
+                supportFragmentManager
+
+            val fragmentTransaction : FragmentTransaction =
+                fragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.frame,SecondFragment())
+            fragmentTransaction.commit()
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
